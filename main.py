9b956d05d935340621ad0df65b9639ea0d592bf6
AbdulMannan <abdulmanan291003@gmail.com>
2026-05-16 19:34:39 +0500

    Added requirements file

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
 import base64
+from colorama import init, Fore
+
+# Initialize colorama
+init()
 
 while True:
 
-    print("\n=== Base64 Encoder/Decoder ===")
-    print("1. Encode")
-    print("2. Decode")
-    print("3. Exit")
+    print(Fore.CYAN + "\n=== Base64 Encoder/Decoder ===")
+    print(Fore.YELLOW + "1. Encode")
+    print(Fore.YELLOW + "2. Decode")
+    print(Fore.YELLOW + "3. Exit")
 
-    choice = input("Choose option: ")
+    choice = input(Fore.WHITE + "Choose option: ")
 
     if choice == "1":
 
-        text = input("Enter text: ")
+        text = input(Fore.WHITE + "Enter text: ")
 
         encoded = base64.b64encode(text.encode()).decode()
 
-        print("\nEncoded Text:")
-        print(encoded)
+        print(Fore.GREEN + "\nEncoded Text:")
+        print(Fore.GREEN + encoded)
 
     elif choice == "2":
 
-        text = input("Enter Base64 text: ")
+        text = input(Fore.WHITE + "Enter Base64 text: ")
 
         try:
             decoded = base64.b64decode(text.encode()).decode()
 
-            print("\nDecoded Text:")
-            print(decoded)
+            print(Fore.GREEN + "\nDecoded Text:")
+            print(Fore.GREEN + decoded)
 
         except:
-            print("\nInvalid Base64 input!")
+            print(Fore.RED + "\nInvalid Base64 input!")
 
     elif choice == "3":
 
-        print("Goodbye!")
+        print(Fore.CYAN + "Goodbye!")
         break
 
     else:
-        print("\nInvalid choice!")
+        print(Fore.RED + "\nInvalid choice!")
